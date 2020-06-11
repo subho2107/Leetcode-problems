b26c7fde618356cdb490a0978f1d545e26877f2a
Tirthankar Bhattacharya <shubho2001g@gmail.com>
2020-06-11 19:24:47 +0530

    update

---
+++ June 30 day challenge/sort colors.py
@@ -0,0 +1,19 @@
+from typing import List
+import random
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        low, high, mid = 0, len(nums)-1, 0
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            else:
+                nums[high], nums[mid] = nums[mid], nums[high]
+                high -= 1
+        print(nums)
