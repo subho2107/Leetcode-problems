1bb91444d94c89151c05fff1d27fe49fd395eba3
Tirthankar Bhattacharya <shubho2001g@gmail.com>
2020-06-13 11:05:25 +0530

    update

---
+++ June 30 day challenge/Insert delete get random   O(1).py
@@ -0,0 +1,39 @@
+import random
+class RandomizedSet:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.myStructure = {}
+
+    def insert(self, val: int) -> bool:
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        """
+        if val not in self.myStructure:
+            self.myStructure[val] = 1
+            return True
+        return False
+
+    def remove(self, val: int) -> bool:
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        """
+        if val in self.myStructure:
+            del self.myStructure[val]
+            return True
+        return  False
+
+    def getRandom(self) -> int:
+        """
+        Get a random element from the set.
+        """
+        val = random.choices(list(self.myStructure.keys()))
+        return val
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
