e39e6b766a8cb0699de8dc785d1423b9a95841e4
Tirthankar Bhattacharya <shubho2001g@gmail.com>
2020-06-24 11:09:11 +0530

    june 30 day challenge

---
+++ June 30 day challenge/CountCompleteTreeNodes.java
@@ -0,0 +1,19 @@
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+ }
+
+public class CountCompleteTreeNodes {
+    public int countNodes(TreeNode root) {
+        if (root==null)return 0;
+        return countNodes(root.left) + countNodes(root.right)+1;
+    }
+}
