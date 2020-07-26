e61d530e0aa8ae250be36576f419a24b72853794
Tirthankar Bhattacharya <shubho2001g@gmail.com>
2020-07-26 22:01:50 +0530

    update july 30 day challenge

---
+++ July 30 day challenge/AddDigits.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class AddDigits {
+    public int addDigits(int num) {
+        if(num/10==0)return num;
+        return (num%9==0)?9:num%9;
+    }
+}
