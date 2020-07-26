a4e2089fedc31553234c39684f2f7d6c89719988
Tirthankar Bhattacharya <shubho2001g@gmail.com>
2020-07-26 12:31:28 +0530

    update july 30 day challenge

--- July 30 day challenge/AllPathsFromSourceToTarget.java
+++ July 30 day challenge/AllPathsFromSourceToTarget.java
@@ -4,10 +4,7 @@ import java.util.*;
 
 public class AllPathsFromSourceToTarget {
     List<List<Integer>> paths;
-    Set<Integer>visited;
     public void dfs(int index, int[][]graph, ArrayList<Integer>currPath){
-        if (index!=graph.length-1)
-            this.visited.add(index);
         currPath.add(index);
         if (graph[index].length==0){
             if (index==graph.length-1) {
@@ -18,16 +15,13 @@ public class AllPathsFromSourceToTarget {
         else {
             for (int neighbour :
                     graph[index]) {
-                if (!this.visited.contains(neighbour))
-                    dfs(neighbour, graph, currPath);
+                dfs(neighbour, graph, currPath);
                 currPath.remove(currPath.size()-1);
             }
         }
     }
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        int len = graph.length;
         paths = new ArrayList<>();
-        visited = new HashSet<>();
         ArrayList<Integer>currPath= new ArrayList<>();
         dfs(0, graph, currPath);
         return paths;
@@ -36,7 +30,7 @@ public class AllPathsFromSourceToTarget {
     public static void main(String args[]) throws Exception {
         BufferedReader bu = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int [][] graph = {{4,3,1}, {3,2,4}, {3}, {}};
+        int [][] graph = {{4,3,1}, {3,2,4}, {3},{4}, {}};
         System.out.println(new AllPathsFromSourceToTarget().allPathsSourceTarget(graph));
         System.out.print(sb);
     }
