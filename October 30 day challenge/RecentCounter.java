a75edbbefa1e600296de55c177a80faa4296a730
Tirthankar Bhattacharya <shubho2001g@gmail.com>
2020-10-02 11:09:22 +0530

    upadte october 30 day challenge

--- October 30 day challenge/RecentCounter.java
+++ October 30 day challenge/RecentCounter.java
@@ -3,17 +3,17 @@ import java.util.*;
 
 class RecentCounter {
     int cnt;
-    ArrayList<Integer>queryTimes;
+    LinkedList<Integer>queryTimes;
     public RecentCounter() {
         cnt = 0;
-        queryTimes = new ArrayList<>();
+        queryTimes = new LinkedList<>();
     }
 
     public int ping(int t) {
         queryTimes.add(t);
         int num = queryTimes.get(queryTimes.size()-1);
         while (queryTimes.get(0)<num-3000)
-            queryTimes.remove(0);
+            queryTimes.removeFirst();
         cnt = queryTimes.size();
         return cnt;
     }
