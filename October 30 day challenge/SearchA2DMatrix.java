0bae5a4b6c2aae604b80f166a55760e653bcd502
Tirthankar Bhattacharya <shubho2001g@gmail.com>
2020-10-17 10:20:51 +0530

    update october 30 day challenge

--- October 30 day challenge/SearchA2DMatrix.java
+++ October 30 day challenge/SearchA2DMatrix.java
@@ -3,14 +3,22 @@ import java.util.*;
 
 public class SearchA2DMatrix {
     public boolean searchMatrix(int[][] matrix, int target) {
-        if( matrix.length == 0||matrix[0].length == 0)
+        if (matrix.length == 0 || matrix[0].length == 0)
             return false;
-        for (int[] ints : matrix) {
-            if (target >= ints[0] && target <= ints[matrix[0].length - 1]) {
-                int pos = Arrays.binarySearch(ints, target);
-                if (pos >= 0 && pos < matrix[0].length && ints[pos] == target)
+        int n = matrix.length, m = matrix[0].length;
+        int startRow = 0, endRow = n - 1;
+        while (startRow <= endRow) {
+            int mid = (startRow + endRow) / 2;
+            if (target >= matrix[mid][0] && target <= matrix[mid][m - 1]) {
+                int index = Arrays.binarySearch(matrix[mid], target);
+                if (index >= 0 && index < m && matrix[mid][index] == target)
                     return true;
-            }
+                else
+                    return false;
+            } else if (target < matrix[mid][0])
+                endRow = mid - 1;
+            else
+                startRow = mid + 1;
         }
         return false;
     }
