9bbfc0f20bab7a7ccbef9915f3bec539f34622e6
Tirthankar Bhattacharya <shubho2001g@gmail.com>
2020-11-04 11:39:14 +0530

    update october 30 day challenge

--- October 30 day challenge/RecoverBinarySearchTree.java
+++ October 30 day challenge/RecoverBinarySearchTree.java
@@ -1,50 +1,47 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
-import java.io.*;
+
+
 import java.util.*;
+
 class RecoverBinarySearchTree {
-    ArrayList<TreeNode> inorder;
-    public void dfs(TreeNode root){
-        if(root != null){
-            if(root.left != null)
-                dfs(root.left);
-            inorder.add(root);
-            if(root.right != null)
-                dfs(root.right);
+    ArrayList<TreeNode[]> irregularities;
+    TreeNode prev;
+
+    public void inorder(TreeNode root) {
+        if (root != null) {
+            inorder(root.left);
+            if (prev != null && root.val < prev.val)
+                irregularities.add(new TreeNode[]{prev, root});
+            prev = root;
+            inorder(root.right);
         }
     }
-    public void recoverTree(TreeNode root) {
-        inorder = new ArrayList<>();
-        dfs(root);
-        TreeNode one = null, two = null;
-        for(int i = 0; i < inorder.size(); i++){
-            TreeNode node = inorder.get(i);
-            if(i == 0 && node.val > inorder.get(i+1).val)
-                one = node;
-            else if(i == inorder.size() - 1 && node.val < inorder.get(i-1).val)
-                two = node;
-            else if(i != 0 && i != inorder.size() - 1  && (node.val < inorder.get(i-1).val || node.val > inorder.get(i+1).val))
-            {
-                if(one == null)
-                    one = node;
-                else
-                    two = node;
-            }
 
+    public void recoverTree(TreeNode root) {
+        inorder(root);
+        TreeNode node1, node2;
+        if (irregularities.size() == 1) {
+            node1 = irregularities.get(0)[0];
+            node2 = irregularities.get(0)[1];
+
+        } else {
+            node1 = irregularities.get(0)[0];
+            node2 = irregularities.get(1)[1];
         }
-        one.val = one.val^two.val^(two.val = one.val);
+        node1.val = node1.val ^ node2.val ^ (node2.val = node1.val);
     }
 }
