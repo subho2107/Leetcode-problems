bbd36688071835b292528097a4076e8fdddd9fab
Tirthankar Bhattacharya <shubho2001g@gmail.com>
2020-11-27 12:20:32 +0530

    update november 30 day challenge

--- November 30 day challenge/LongestSubstringWithAtLeastKRepeatingCharacters.java
+++ November 30 day challenge/LongestSubstringWithAtLeastKRepeatingCharacters.java
@@ -5,22 +5,35 @@ public class LongestSubstringWithAtLeastKRepeatingCharacters {
     public int longestSubstring(String s, int k) {
         int longestSubstring = 0, n = s.length();
         int[] freq = new int[26];
-        boolean check;
-        for (int i = 0; i < n; i++) {
+        HashSet<Character>uniques = new HashSet<>();
+        for (char c : s.toCharArray()) {
+            uniques.add(c);
+        }
+        int uniqueNums = uniques.size();
+        for (int currUnique = 1; currUnique <= uniqueNums; currUnique++) {
             Arrays.fill(freq, 0);
-            for (int j = i; j < n; j++) {
-                freq[s.charAt(j) - 'a']++;
-                check = true;
-                for (int num : freq) {
-                    if (num == 0)
-                        continue;
-                    if (num < k) {
-                        check = false;
-                        break;
-                    }
+            int start = 0, end = 0, uniqueChars = 0, countAtLeastK = 0;
+            while (end < n){
+
+                if (uniqueChars <= currUnique){
+                    int pos = s.charAt(end)-'a';
+                    if (freq[pos] == 0)uniqueChars++;
+                    freq[pos]++;
+                    if (freq[pos] == k)
+                        countAtLeastK++;
+                    end++;
                 }
-                if (check)
-                    longestSubstring = Math.max(longestSubstring, j - i + 1);
+                else {
+                    int pos = s.charAt(start)-'a';
+                    if (freq[pos]== k)
+                        countAtLeastK--;
+                    freq[pos]--;
+                    if (freq[pos] == 0)
+                        uniqueChars--;
+                    start++;
+                }
+                if (uniqueChars == currUnique && uniqueChars == countAtLeastK)
+                    longestSubstring = Math.max(longestSubstring, end-start);
             }
         }
         return longestSubstring;
