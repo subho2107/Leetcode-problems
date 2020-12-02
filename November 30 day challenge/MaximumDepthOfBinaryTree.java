c402754da020f30dabfe3754d0943e4e7a37ba6e
Tirthankar Bhattacharya <shubho2001g@gmail.com>
2020-12-02 16:17:42 +0530

    update november 30 day challenge

---
+++ November 30 day challenge/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class MaximumDepthOfBinaryTree {
+    int max_depth;
+    public void dfs(TreeNode node, int depth){
+        if(node == null)
+            return;
+        if(node.left == null && node.right == null)
+            max_depth = Math.max(max_depth, depth);
+        dfs(node.left, depth+1);
+        dfs(node.right, depth+1);
+    }
+    public int maxDepth(TreeNode root) {
+        if(root == null)
+            return 0;
+        max_depth = 0;
+        dfs(root, 0);
+        return max_depth+1;
+    }
+}
