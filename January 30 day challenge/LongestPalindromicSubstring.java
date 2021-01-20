5dce30aa3572714eb037fcd958ee7b2a7e9c95e9
Tirthankar Bhattacharya <shubho2001g@gmail.com>
2021-01-20 14:21:25 +0530

    update january 30 day challenge

---
+++ January 30 day challenge/LongestPalindromicSubstring.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class LongestPalindromicSubstring {
+    public String longestPalindrome(String s) {
+        int maxLen = 1;
+        String maxString = " ";
+        for(int mid = 0; mid < s.length(); mid++){
+            int i = mid, j = mid;
+            while(i >=0 && j < s.length() && s.charAt(i) == s.charAt(j)){
+                i--;
+                j++;
+            }
+            if(j-i-1 > maxLen){
+                maxLen = j - i - 1;
+                maxString = s.substring(i+1, j);
+            }
+            i = mid; j = mid+1;
+            while(i >=0 && j < s.length() && s.charAt(i) == s.charAt(j)){
+                i--;
+                j++;
+            }
+            if(j-i-1 > maxLen){
+                maxLen = j - i - 1;
+                maxString = s.substring(i+1, j);
+            }
+        }
+        return maxString;
+    }
+    public static void main(String[] args) throws Exception {
+        System.out.println(new LongestPalindromicSubstring().longestPalindrome("babad"));
+    }
+}
