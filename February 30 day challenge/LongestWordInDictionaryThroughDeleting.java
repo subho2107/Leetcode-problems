7990f34ba983e47cafb9fadcc888c896919b3240
Tirthankar Bhattacharya <shubho2001g@gmail.com>
2021-02-23 13:26:52 +0530

    update february 30 day challenge

--- February 30 day challenge/LongestWordInDictionaryThroughDeleting.java
+++ February 30 day challenge/LongestWordInDictionaryThroughDeleting.java
@@ -23,13 +23,4 @@ public class LongestWordInDictionaryThroughDeleting {
         }
         return "";
     }
-    public static void main(String[] args) throws Exception {
-        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int t = Integer.parseInt(buffer.readLine());
-        while (t-- > 0) {
-
-        }
-        System.out.println(sb);
-    }
 }
