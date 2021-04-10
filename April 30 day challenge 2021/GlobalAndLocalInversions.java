8d64107c85c8c26a538850100f779a51dee5c419
Tirthankar Bhattacharya <shubho2001g@gmail.com>
2021-04-10 12:36:14 +0530

    update april 30 day challenge

---
+++ April 30 day challenge 2021/GlobalAndLocalInversions.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class GlobalAndLocalInversions {
+    public boolean isIdealPermutation(int[] nums) {
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            if(Math.abs(nums[i] - i) > 1)
+                return false;
+        }
+        return true;
+    }
+}
