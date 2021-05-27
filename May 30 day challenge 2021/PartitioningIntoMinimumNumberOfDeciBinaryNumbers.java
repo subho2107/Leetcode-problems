f103371db5f4b75a71db4fcd7ebb0d82ffcf21cd
Tirthankar Bhattacharya <shubho2001g@gmail.com>
2021-05-27 12:40:45 +0530

    update may 30 day challenge

---
+++ May 30 day challenge 2021/PartitioningIntoMinimumNumberOfDeciBinaryNumbers.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class PartitioningIntoMinimumNumberOfDeciBinaryNumbers {
+    public int minPartitions(String n) {
+        int max = 0;
+        for(char ch : n.toCharArray())
+            max = Math.max(max, ch-'0');
+        return max;
+    }
+}
