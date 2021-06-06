f53aa9ac588d1862131c0b180542628f99641a65
Tirthankar Bhattacharya <shubho2001g@gmail.com>
2021-06-07 03:16:13 +0530

    update june 30 day challenge

--- June 30 day challenge 2021/MaximumPerformanceOfATeam.java
+++ June 30 day challenge 2021/MaximumPerformanceOfATeam.java
@@ -24,13 +24,4 @@ public class MaximumPerformanceOfATeam {
         }
         return (int)(ans%MOD);
     }
-    public static void main(String[] args) throws Exception {
-        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int t = Integer.parseInt(buffer.readLine());
-        while (t-- > 0) {
-
-        }
-        System.out.println(sb);
-    }
 }
