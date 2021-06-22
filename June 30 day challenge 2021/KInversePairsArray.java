fa012a6c02bfd43ed1e1f940db3e1aceed746c31
Tirthankar Bhattacharya <shubho2001g@gmail.com>
2021-06-22 21:56:00 +0530

    update june 30 day challenge

--- June 30 day challenge 2021/KInversePairsArray.java
+++ June 30 day challenge 2021/KInversePairsArray.java
@@ -3,19 +3,18 @@ import java.util.*;
 
 public class KInversePairsArray {
     public int kInversePairs(int n, int k) {
-        int[][] dp = new int[n+1][k+1];
-        dp[1][0] = 1;
+        int [] dp = new int[k+1];
         int MOD = 1000_000_007;
-        for(int row = 1; row < n; row++){
-            int maxK = (row*(row+1)/2)-row;
-            for(int col = 0; col <= Math.min(k, maxK); col++)
-            {
-                for(int i = 0; i <= row && col+i <= k; i++){
-                    dp[row+1][col+i] += dp[row][col];
-                    dp[row+1][col+i] %= MOD;
-                }
+        for(int i = 1; i <= n; i++){
+            int [] temp = new int[k+1];
+            temp[0] = 1;
+            for(int j = 1; j <= k; j++){
+                int val = (dp[j] + MOD - (j-i >= 0? dp[j-i]:0))%MOD;
+                temp[j] = (val + temp[j-1])%MOD;
+
             }
+            dp = temp;
         }
-        return dp[n][k];
+        return ((dp[k] + MOD - (k > 0 ? dp[k - 1] : 0)) % MOD);
     }
 }
