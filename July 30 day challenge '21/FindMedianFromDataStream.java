464b78be75b13e81b29f8b066439041d1f911503
Tirthankar Bhattacharya <shubho2001g@gmail.com>
2021-07-12 13:58:34 +0530

    update july 30 day challenge

--- July 30 day challenge '21/FindMedianFromDataStream.java
+++ July 30 day challenge '21/FindMedianFromDataStream.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class FindMedianFromDataStream {
+class MedianFinder {
     PriorityQueue<Integer>minHeap, maxHeap;
     public MedianFinder() {
         minHeap = new PriorityQueue<>();
