b399b55bb60d6388bb26a6a41d617cd71f5cdeca
Tirthankar Bhattacharya <shubho2001g@gmail.com>
2021-07-16 01:35:35 +0530

    update july 30 day challenge

--- July 30 day challenge '21/LongestIncreasingSubsequence.java
+++ July 30 day challenge '21/LongestIncreasingSubsequence.java
@@ -2,19 +2,35 @@ import java.io.*;
 import java.util.*;
 
 public class LongestIncreasingSubsequence {
-    public int lengthOfLIS(int[] arr) {
-        int n = arr.length;
-        int [] dp = new int[n];
-        Arrays.fill(dp, 1);
-        int ans = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = i-1; j>= 0; j--){
-                if(arr[j] < arr[i] && dp[j] + 1 > dp[i]){
-                    dp[i] = dp[j] + 1;
-                }
-            }
-            ans = Math.max(ans, dp[i]);
+    public int getCeilPosition(int [] arr, int num, int n){
+        int low = 0, high = n-1;
+        while(low < high){
+            int mid = (low + high)/2;
+            if(num <= arr[mid])
+                high = mid;
+            else
+                low = mid+1;
         }
-        return ans;
+        return high;
+    }
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int [] piles = new int[n];
+        int len = 1;
+        piles[0] = nums[0];
+        for(int i = 1; i < n; i++){
+            if(nums[i] < piles[0])
+                piles[0] = nums[i];
+            else if(nums[i] > piles[len-1])
+                piles[len++] = nums[i];
+            else
+                piles[getCeilPosition(piles, nums[i], len)] = nums[i];
+        }
+        return len;
+    }
+
+    public static void main(String[] args)throws Exception{
+        int [] arr = {10,9,2,5,3,7,101,18};
+        System.out.println(new LongestIncreasingSubsequence().lengthOfLIS(arr));
     }
 }
