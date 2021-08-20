a4173432bebd1970321d04edfacd3d95855b70a5
subho2107 <shubho2001g@gmail.com>
2021-08-21 02:15:48 +0530

    update august 30 day challenge

--- August 30 day challenge '21/MaximumProductOfSplittedBinaryTree.java
+++ August 30 day challenge '21/MaximumProductOfSplittedBinaryTree.java
@@ -2,27 +2,23 @@ import java.io.*;
 import java.util.*;
 
 public class MaximumProductOfSplittedBinaryTree {
-    Map<TreeNode, Long>subtreeSize;
-    public void calcSize(TreeNode node){
-        if(node == null)
-            return;
-        long currSize = node.val;
-        calcSize(node.left);
-        calcSize(node.right);
-        currSize += subtreeSize.get(node.left)+subtreeSize.get(node.right);
-        subtreeSize.put(node, currSize);
-    }
-    public long getMaxProd(TreeNode node, long size){
+    long ans,total;
+    public long getMaxProd(TreeNode node){
         if(node == null)
             return 0;
-        long currSize = subtreeSize.get(node);
-        return Math.max((size-currSize)*currSize, Math.max(getMaxProd(node.left, size), getMaxProd(node.right, size)));
+        long currVal = node.val;
+        long left = getMaxProd(node.left);
+        long right = getMaxProd(node.right);
+        currVal += left+right;
+        ans = Math.max(ans, Math.max((total-left)*left, (total-right)*right));
+        return currVal;
     }
     public int maxProduct(TreeNode root) {
         int MOD = (int)(1e9+7);
-        subtreeSize = new HashMap<>();
-        subtreeSize.put(null, (long)0);
-        calcSize(root);
-        return (int)(getMaxProd(root, subtreeSize.get(root))%MOD);
+        ans = 0;
+        total = 0;
+        total = getMaxProd(root);
+        getMaxProd(root);
+        return (int)(ans%MOD);
     }
 }
