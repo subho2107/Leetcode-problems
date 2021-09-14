bc515dc93b7f65929c36d977cab11ec8bbbfcf37
subho2107 <shubho2001g@gmail.com>
2021-09-14 19:49:18 +0530

    update august 30 day challenge

---
+++ August 30 day challenge '21/PatchingArray.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class PatchingArray {
+    public int minPatches(int[] nums, int n) {
+        long miss = 1;
+        int added = 0, i = 0;
+        while(miss <= n){
+            if(i < nums.length && nums[i] <= miss)
+                miss += nums[i++];
+            else{
+                miss *= 2;
+                added++;
+            }
+        }
+        return added;
+    }
+}
