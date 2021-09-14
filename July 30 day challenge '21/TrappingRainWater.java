7575488ee3b0b7a82eac824283c3d6fc455fd354
subho2107 <shubho2001g@gmail.com>
2021-09-14 17:39:56 +0530

    update july 30 day challenge

--- July 30 day challenge '21/TrappingRainWater.java
+++ July 30 day challenge '21/TrappingRainWater.java
@@ -24,5 +24,20 @@ public class TrappingRainWater {
         }
         return ans;
     }
-
+    public int trap2(int[] height) {
+        Stack<Integer>stack = new Stack<>();
+        int n = height.length, ans = 0;
+        for(int i = 0; i < n; i++){
+            while(!stack.isEmpty() && height[i] > height[stack.peek()]){
+                int top = stack.pop();
+                if(stack.isEmpty())
+                    break;
+                int dist = i-stack.peek()-1;
+                int currWaterHeight = Math.min(height[i], height[stack.peek()])-height[top];
+                ans += dist*currWaterHeight;
+            }
+            stack.push(i);
+        }
+        return ans;
+    }
 }
