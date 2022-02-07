8e60b5d81d909b98d34d4afe28591f461ec0b06a
subho2107 <shubho2001g@gmail.com>
2022-02-08 02:51:23 +0530

    update february 30 day challenge

--- February 30 day challenge 2022/FourSumIIFeb22.java
+++ February 30 day challenge 2022/FourSumIIFeb22.java
@@ -1,6 +1,5 @@
-import java.io.*;
 import java.util.*;
-public class FourSumIIJan22 {
+public class FourSumIIFeb22 {
     public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
         int n = nums1.length, cnt = 0;
         HashMap<Integer, Integer>ab = new HashMap<>(), cd = new HashMap<>();
